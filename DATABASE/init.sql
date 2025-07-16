fe66c9f2498dc5707606bec8e8cd729ea877fff7
Mikemaranon <mikemaranonx@gmail.com>
2025-07-16 21:10:55 +0200

    DATABASE params added

--- DATABASE/init.sql
+++ DATABASE/init.sql
@@ -1,9 +1,19 @@
 -- DATABASE/init.sql
 
+-- VARIABLES
+CREATE TYPE user_role AS ENUM ('admin', 'trainer', 'developer', 'user');
+
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
     password TEXT NOT NULL,
-    role VARCHAR(20) NOT NULL,
+    role user_role NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE sessions (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES users(id),
+  token TEXT,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
